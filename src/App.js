4310cbea4656a66229f34ff19dcfd9626ae8af9a
ZICCORP <frankchuka250@gmail.com>
2022-02-11 04:41:03 +0100

    imported the Navbar and CartContainer component into the App component

--- src/App.js
+++ src/App.js
@@ -1,8 +1,13 @@
-import React from 'react'
+import React from 'react';
+import Navbar from './Navbar';
+import CartContainer from './CartContainer';
 
 function App() {
   return <>
-    <h1>Awesome Shopping Cart</h1>
+    <main>
+      <Navbar />
+      <CartContainer />
+    </main>
   </>
 }
 
